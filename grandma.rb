779d6abf7d83777beca5bdeb8b246d77a0d1cb27
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 16:49:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
+    elsif phrase == phrase.upcase
+    return 
     
 
 end 
