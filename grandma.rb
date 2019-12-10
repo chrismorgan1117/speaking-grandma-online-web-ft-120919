388d68f73f7dc93f7e47592064533f3d602135e1
Christopher Morgan <cwm111794@gmail.com>
2019-12-10 16:48:20 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,5 @@
-def speak_to_grandma()
-  if speak_to_grandma("I LOVE YOU GRANDMA!")
-    puts "I LOVE YOU TOO PUMPKIN!"
-    elsif speak_to_grandma() == speak_to_grandma().upcase
-    puts "NO, NOT SINCE 1938"
-  else
-    puts "HUH?! SPEAK UP SONNY!"
-end
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA!"
+    "I LOVE YOU TOO!"
+
 end 
